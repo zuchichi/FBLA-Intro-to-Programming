c6e92e844e3e90b889db3539dba7d29db62897fa
UchechiE <uchechie19@gmail.com>
2026-03-11 05:14:24 -0500

    Added cache into App.jsx.

--- Extension/extension/src/App.jsx
+++ Extension/extension/src/App.jsx
@@ -1,6 +1,7 @@
 {/* Important Imports*/}
 import { useState } from 'react'
 import { HashRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
+import { UserProvider } from './context/UserContext'; // Cache
 
 {/* Importing Components*/}
 import './App.css'
@@ -28,23 +29,25 @@ function Layout() {
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Intro />} />
-          <Route path="/intro" element={<Intro />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/customize" element={<Customize />} />
-          <Route path="/pet" element={<Pet />} />
-          <Route path="/petstats" element={<PetStats />} />
-          <Route path="/marketplace" element={<Marketplace />} />
-          <Route path="/financialogistics" element={<FinancialLogistics />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/chores" element={< Chores />} />
-        </Route>
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Intro />} />
+            <Route path="/intro" element={<Intro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/customize" element={<Customize />} />
+            <Route path="/pet" element={<Pet />} />
+            <Route path="/petstats" element={<PetStats />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/financialogistics" element={<FinancialLogistics />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/chores" element={< Chores />} />
+          </Route>
+        </Routes>
+      </Router>
+    </UserProvider>
   )
 }
 
